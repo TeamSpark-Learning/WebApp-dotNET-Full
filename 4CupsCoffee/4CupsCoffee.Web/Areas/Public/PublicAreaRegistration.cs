b270ca077d77702afc49dd71c8b15d9950dff4a8
Boyko Anton <boyko.ant@live.com>
2015-09-09 18:47:03 +0300

    initialize project

---
+++ 4CupsCoffee/4CupsCoffee.Web/Areas/Public/PublicAreaRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace _4CupsCoffee.Web.Areas.Public
+{
+    public class PublicAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Public";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Public_HomePage",
+                "",
+                new { controller = "Home", action = "Index" }
+            );
+
+            context.MapRoute(
+                "Public_Default",
+                "Public/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
